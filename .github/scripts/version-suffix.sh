992279dd46097102e8a092beeb93ff30b1068261
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-20 22:11:34 +0100

    fix coding style

--- .github/scripts/version-suffix.sh
+++ .github/scripts/version-suffix.sh
@@ -8,17 +8,17 @@ HOTFIX_REGEX="refs/heads/hotfix/.*"
 BUGFIX_REGEX="refs/heads/bugfix/.*"
 
 if [[ "${GITHUB_REF}" =~ ${FEATURE_REGEX} ]]; then
-  VERSION_SUFIX="feature-${GITHUB_SHA_SHORT}"
+  VERSION_SUFFIX="feature-${GITHUB_SHA_SHORT}"
 elif [[ "${GITHUB_REF}" =~ ${HOTFIX_REGEX} ]]; then
-  VERSION_SUFIX="hotfix-${GITHUB_SHA_SHORT}"
+  VERSION_SUFFIX="hotfix-${GITHUB_SHA_SHORT}"
 elif [[ "${GITHUB_REF}" =~ ${BUGFIX_REGEX} ]]; then
-  VERSION_SUFIX="bugfix-${GITHUB_SHA_SHORT}"
+  VERSION_SUFFIX="bugfix-${GITHUB_SHA_SHORT}"
 elif [[ "${GITHUB_REF}" =~ ${RELEASE_REGEX} ]]; then
-  VERSION_SUFIX="bugfix-${GITHUB_SHA_SHORT}"
+  VERSION_SUFFIX="bugfix-${GITHUB_SHA_SHORT}"
 else
-  VERSION_SUFIX="develop-${GITHUB_SHA_SHORT}"
+  VERSION_SUFFIX="develop-${GITHUB_SHA_SHORT}"
 fi
 
-export VERSION_SUFIX
+export VERSION_SUFFIX
 
-echo "Version Sufix: ${VERSION_SUFIX}"
+echo "Version Sufix: ${VERSION_SUFFIX}"
