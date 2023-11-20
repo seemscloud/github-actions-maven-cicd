a84c6e88a631823f385938dc7a3e57b13234e605
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-11-20 22:08:12 +0100

    fix coding style

--- .github/scripts/short-commit.sh
+++ .github/scripts/short-commit.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "${GITHUB_SHA}" | head -c 8
+GITHUB_SHA_SHORT=$(echo "${GITHUB_SHA}" | head -c 8)
+
+MAIN_REGEX="refs/heads/main"
+RELEASE_REGEX="refs/heads/release/.*"
+FEATURE_REGEX="refs/heads/feature/.*"
+HOTFIX_REGEX="refs/heads/hotfix/.*"
+BUGFIX_REGEX="refs/heads/bugfix/.*"
+
+if [[ "${GITHUB_REF}" =~ ${MAIN_REGEX} ]] ; then
+  VERSION_SUFIX="main"
+elif [[ "${GITHUB_REF}" =~ ${FEATURE_REGEX} ]]; then
+  VERSION_SUFIX="feature-${GITHUB_SHA_SHORT}"
+elif [[ "${GITHUB_REF}" =~ ${HOTFIX_REGEX} ]]; then
+  VERSION_SUFIX="hotfix-${GITHUB_SHA_SHORT}"
+elif [[ "${GITHUB_REF}" =~ ${BUGFIX_REGEX} ]]; then
+  VERSION_SUFIX="bugfix-${GITHUB_SHA_SHORT}"
+elif [[ "${GITHUB_REF}" =~ ${RELEASE_REGEX} ]]; then
+  VERSION_SUFIX="bugfix-${GITHUB_SHA_SHORT}"
+else
+  VERSION_SUFIX="develop-${GITHUB_SHA_SHORT}"
+fi
+
+export VERSION_SUFIX
